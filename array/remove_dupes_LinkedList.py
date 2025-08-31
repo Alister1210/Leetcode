f276cdaffae174c9c60f06283126db52fb6b6be3
Alister Fernandes <alisterfernandes1210@gmail.com>
2025-08-31 11:03:36 +0530

    REMOVE THE DUPLICATE NUMBER IN LINKED LIST

---
+++ array/remove_dupes_LinkedList.py
@@ -0,0 +1,9 @@
+# 83. Remove Duplicates from Sorted List
+def deleteDuplicates(self, head):
+    current = head
+    while current and current.next:
+        if current.val == current.next.val:
+            current.next= current.next.next
+        else:
+            current = current.next
+    return head
