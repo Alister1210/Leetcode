b6b31f77f542ef4beaa9738247d0a65a0313fe4b
Alister Fernandes <alisterfernandes1210@gmail.com>
2025-09-01 13:58:22 +0530

    Anagram Grouping_ 2 more solutions added

--- strings/anagram_grouping.py
+++ strings/anagram_grouping.py
@@ -16,4 +16,35 @@ class Solution(object):
         print(final_list) #[['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
         return final_list
         
+
 # Solution 2 (Optimal solution) uses char freq counting method
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagram_grps = {}
+
+        for string in sorted(strs):
+            char_count = [0]*26
+            for s in string:
+                char_count[ord(s) - ord('a')] +=1
+            key = tuple(char_count) #list cannot be used as keys
+            if key not in anagram_grps:
+                anagram_grps[key] = []
+            anagram_grps[key].append(string)
+
+        final_list = list(anagram_grps.values())
+        return final_list
+        
+# Solution 3 using defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagram_grps = defaultdict(list)
+
+        for string in sorted(strs):
+            char_count = [0]*26
+            for i in string:
+                char_count[ord(i) - ord('a')] +=1
+
+            anagram_grps[tuple(char_count)].append(string)
+
+        return anagram_grps.values()
