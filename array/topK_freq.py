6e47f5e3c5bfdf9dc59bc786b65fa64294720759
Alister Fernandes <alisterfernandes1210@gmail.com>
2025-09-02 11:37:19 +0530

    Top k Frequent Elements Solution2

--- array/topK_freq.py
+++ array/topK_freq.py
@@ -1,3 +1,5 @@
+#  Solution 1 using python inbuilt sort method and hashmap or dict to store count of each num in array
+from collections import defaultdict
 class Solution(object):
     def topKFrequent(self, nums, k):
         freq = defaultdict(int)
@@ -11,3 +13,23 @@ class Solution(object):
         final_list = [i for i,k in sorted_dict[:k]]
         return final_list
     
+# Solution 2 Using heap to sort(Hashmap + Priority Queue)
+from collections import defaultdict
+import heapq
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        freq = defaultdict(int)
+        for num in nums:
+            freq[num] +=1
+
+        pq = [(-count, key) for key,count in freq.items()]
+        print(pq)
+        heapq.heapify(pq)
+        print(pq)
+
+        result = []
+        for _ in range(k):
+            count , val = heapq.heappop(pq)
+            result.append(val)
+        return result
+                
